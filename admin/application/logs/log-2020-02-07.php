bcab48e6e881f324d13671a092378d6341e70544
devteam3-dyits <60871919+devteam3-dyits@users.noreply.github.com>
2020-02-11 10:47:23 +0800

    changes in git ignore file

--- admin/application/logs/log-2020-02-07.php
+++
@@ -1,6 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2020-02-07 01:19:14 --> Could not find the language line "email"
-ERROR - 2020-02-07 01:19:14 --> Could not find the language line "password"
-ERROR - 2020-02-07 01:25:32 --> Could not find the language line "email"
-ERROR - 2020-02-07 01:25:32 --> Could not find the language line "password"
